1e091c1aed7556db1632aa217a1336c6f0ca18cf
Owner <zoomer1975@gmail.com>
2026-03-19 21:07:44 +0900

    Add PM2 configuration and log ignore

---
+++ ecosystem.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  apps: [
+    {
+      name: 'stat-saju-proxy',
+      script: 'server.js',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      env: {
+        NODE_ENV: 'production',
+      },
+      error_file: 'logs/err.log',
+      out_file: 'logs/out.log',
+      log_date_format: 'YYYY-MM-DD HH:mm:ss',
+    },
+  ],
+};
